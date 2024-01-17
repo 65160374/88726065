9e4f5b24dd5d55162ebaaa1ac94e3bd6cf3a4656
65160374 <156106990+65160374@users.noreply.github.com>
2024-01-17 18:53:42 +0700

    commit#3

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -18,16 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
     todoInput.value = "";
     }
     }
-    // ลบรายการ Todo
-    function deleteTodo(index) {
-    todos.splice(index, 1);
-    renderTodoList();
-    }
-    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
-    function toggleComplete(index) {
-    todos[index].completed = !todos[index].completed;
-    renderTodoList();
-    }
+    // // ลบรายการ Todo
+    // function deleteTodo(index) {
+    // todos.splice(index, 1);
+    // renderTodoList();
+    // }
+    // // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    // function toggleComplete(index) {
+    // todos[index].completed = !todos[index].completed;
+    // renderTodoList();
+    // }
     // แสดงรายการ Todo บนหน้าเว็บ
     function renderTodoList() {
     console.log(todos);
