29542077e999078fc76642e0b1a927fcce4a9d65
65160374 <156106990+65160374@users.noreply.github.com>
2024-01-17 18:55:37 +0700

    commit#4

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -19,11 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     }
     // // ลบรายการ Todo
-    // function deleteTodo(index) {
-    // todos.splice(index, 1);
-    // renderTodoList();
-    // }
-    // // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    function deleteTodo(index) {
+    todos.splice(index, 1);
+    renderTodoList();
+    }
+    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
     // function toggleComplete(index) {
     // todos[index].completed = !todos[index].completed;
     // renderTodoList();
